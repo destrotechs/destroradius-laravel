aff4fdcda2e43b185a7ff3bcbdc09ade728bea9b
destro <destrotechs@git.com>
2022-05-07 16:50:06 +0300

    added pear router os package

--- app/Http/Controllers/paymentsController.php
+++ app/Http/Controllers/paymentsController.php
@@ -3,10 +3,26 @@
 namespace App\Http\Controllers;
 use DB;
 use Illuminate\Http\Request;
-
+use PEAR2\Net\RouterOS;
+// require_once 'PEAR2/Autoload.php';
 class paymentsController extends Controller
 {
     public function selectPayOption(){
+        try {
+            $client = new RouterOS\Client('ADDRESS', 'LOGIN', 'PASSWORD');
+            $responses = $client->sendSync(new RouterOS\Request('/ip/hotspot/active/print'));
+
+            foreach ($responses as $response) {
+                if ($response->getType() === RouterOS\Response::TYPE_DATA) {
+                    echo 'User: ', $response->getProperty('user'),
+                    "\n";
+                }
+            }
+        }
+        catch (Exception $e) {
+            die($e);
+            // echo "failed";
+        }
     	return view('payments.selectoption');
     }
     public function payoption(Request $request){
