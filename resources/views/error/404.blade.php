c0ba6448ec0d93250f9704ac59c20eb6ea991d25
destro <destrotechs@git.com>
2022-06-05 07:28:21 +0300

    modified 404 page

--- resources/views/error/404.blade.php
+++ resources/views/error/404.blade.php
@@ -1,4 +1,36 @@
-@extends('adminlte::page')
-@section('content')
-<div class="alert alert-danger"><h1>Page NOT found</h1></div>
-@endsection
+<!doctype html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <!-- Bootstrap CSS -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+
+    <title>PAGE NOT FOUND</title>
+  </head>
+  <body>
+    <div class="container-fluid bg-light">
+    	<div class="d-flex justify-content-center">
+    		<div class="text-center mt-5">
+    		<h1 class="flex-column text-danger">404 </h1><br>
+    		<h2 class="flex-column">OOPs!! </h2><br>
+    		<h3 class="text-muted">WE COULD NOT FIND THE PAGE YOU ARE LOOKING FOR!!!</h3><br>
+    		<h6><a href="#" class="btn btn-lg btn-info">Home</a>&nbsp;<a href="#" class="btn btn-lg btn-info">Contact Support</a></h6>
+    	</div>
+    	</div>
+    </div>
+
+    <!-- Optional JavaScript; choose one of the two! -->
+
+    <!-- Option 1: Bootstrap Bundle with Popper -->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
+
+    <!-- Option 2: Separate Popper and Bootstrap JS -->
+    <!--
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.2/dist/umd/popper.min.js" integrity="sha384-IQsoLXl5PILFhosVNubq5LC7Qb9DXgDA9i+tQ8Zj3iwWAwPtgFTxbJ8NT4GN1R8p" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.min.js" integrity="sha384-cVKIPhGWiC2Al4u+LWgxfKTRIcfu0JTxR+EQDz/bgldoEyl4H0zUF0QKbrJ0EcQF" crossorigin="anonymous"></script>
+    -->
+  </body>
+</html>
