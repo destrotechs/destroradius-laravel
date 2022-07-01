1ce39e22b867be9672bb52093002e414233d56af
robotchiti <myemail@example.com>
2022-07-01 11:42:24 +0300

    changed the sms crds

--- app/Message.php
+++ app/Message.php
@@ -29,13 +29,16 @@ class Message extends Model
                     "Text"=>$Message1
                 ]
             ],
-            "ApiKey"=> "CWQsWeC7x2mS4fjtdiqVeHYBOl/QuccvJjYBsylzn1A=",
-            "ClientId"=> "740825e2-1d22-47bc-95be-3085f20cc608"
+            "ApiKey"=> "+YVA7mZV/OaPoNjThgeKJK1BBOySDFhdrT4XqHh13jY=",
+            // "ApiKey"=> "CWQsWeC7x2mS4fjtdiqVeHYBOl/QuccvJjYBsylzn1A=",
+            "ClientId"=> "91e87d9a-7e43-46e1-8adf-73f1490edcd2"
+            // "ClientId"=> "740825e2-1d22-47bc-95be-3085f20cc608"
         ])     
         ,
           CURLOPT_HTTPHEADER => array(
             'Content-Type: application/json',
             'AccessKey: aTLVmWasvnIkCwp2GtzmnKbUJGUU4qLV',
+            // 'AccessKey: aTLVmWasvnIkCwp2GtzmnKbUJGUU4qLV',
             'Cookie: AWSALBTG=EDhs38fB5oVChP8GV5NFJxwg6gNzRGkDqCN/svOyywhdDKmVBRjrcLna9ijEJiFyEbWqd11m/BoR+QbvAiodXX5RwhCN/TVMf3tUigMGNqlfS5X7a6ZDDR3TIo0aoPE0aNk6SU3NPK87xTHFD1Q30uBmnClSytKz3ygNj8tqLtheX2/YNn4=; AWSALBTGCORS=EDhs38fB5oVChP8GV5NFJxwg6gNzRGkDqCN/svOyywhdDKmVBRjrcLna9ijEJiFyEbWqd11m/BoR+QbvAiodXX5RwhCN/TVMf3tUigMGNqlfS5X7a6ZDDR3TIo0aoPE0aNk6SU3NPK87xTHFD1Q30uBmnClSytKz3ygNj8tqLtheX2/YNn4='
           ),
         ));
